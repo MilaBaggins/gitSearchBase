a88f16781a0f473e4398251a3a8e59588b2216ff
camila de souza <camila.sparquitetura@gmail.com>
2022-10-16 22:30:33 -0300

    first commit

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,1 +1,50 @@
-/* Desenvolva sua lógica aqui...*/
+async function getData(userValue) {
+
+    try {
+        const data = await fetch(`https://api.github.com/users/${userValue}`)
+        const dataJson = await data.json();
+        const userArray = [dataJson.stringify]
+
+        localStorage.setItem("usuario1", JSON.stringify(dataJson))
+        console.log(dataJson)
+        renderRecentProfiles()
+
+    } catch {
+        console.log("Deu ruim!")
+    }
+}
+
+
+
+function setInput() {
+    const buttonInputUser = document.getElementById('buttonSearchUser')
+
+    buttonInputUser.addEventListener('click', async () => {
+        const inputUser = document.getElementById('userInput')
+        inputUser.value;
+        console.log(inputUser.value)
+        await getData(inputUser.value)
+        window.location.href='pages/profile/index.html'
+    })
+}
+setInput()
+
+function renderRecentProfiles() {
+    
+
+    const getItem = localStorage.getItem("usuario1")
+    const parseItem = JSON.parse(getItem)
+
+    const divProfiles = document.getElementById('profiles-div')
+    const smallRecentFoundTitle = document.getElementById('smallTitle')
+    divProfiles.innerHTML = '' 
+
+    const imgRecentProfilePic = document.createElement('img')
+    imgRecentProfilePic.src = (parseItem.avatar_url)
+
+    divProfiles.append(smallRecentFoundTitle, imgRecentProfilePic)
+    
+}
+renderRecentProfiles() 
+
+
--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,1 +1,104 @@
-/* Desenvolva sua lógica aqui...*/
+function creatCard(dataRepo){
+    const cardsSection = document.getElementById('cards')
+
+    const cardsRepository = document.createElement('div')
+    cardsRepository.classList.add('cards-repository')
+    cardsRepository.classList.add('container')
+
+    const h3ProjectModule = document.createElement('h3')
+    h3ProjectModule.innerText = (dataRepo.name)
+    
+    const pRepository = document.createElement('p')
+    pRepository.classList.add('repository-p')
+    pRepository.innerText = (dataRepo.description)
+
+    const buttonRepository = document.createElement('button')
+    buttonRepository.setAttribute('id', 'button-repository')
+    buttonRepository.setAttribute('onclick', `location.href='${dataRepo.html_url}'`)
+    buttonRepository.innerText = ('Repositório')
+
+    const buttonDemo = document.createElement('button')
+    buttonDemo.setAttribute('id', 'demo-button')
+    buttonDemo.innerText = ('Demo')
+
+    cardsRepository.append(h3ProjectModule, pRepository, buttonRepository, buttonDemo)
+    cardsSection.appendChild(cardsRepository)
+
+
+    return cardsSection
+}
+
+
+
+function renderRecentProfiles() {
+    
+    const getItem = localStorage.getItem("usuario1")
+    const parseItem = JSON.parse(getItem)
+    console.log(parseItem)
+
+    const boxProfile = document.getElementById('box-profile')
+    boxProfile.innerHTML = '' 
+
+    const imgRecentProfilePic = document.createElement('img')
+    imgRecentProfilePic.src = (parseItem.avatar_url)
+
+    const profileInfo = document.createElement('div')
+    profileInfo.classList.add('profile-info')
+
+    const h3UserName = document.createElement('h3')
+    h3UserName.classList.add('user-name')
+    h3UserName.innerText = (parseItem.name)
+
+    const pUserOcupation = document.createElement('p')
+    pUserOcupation.classList.add('user-ocupation')
+    pUserOcupation.innerText = (parseItem.bio)
+
+    profileInfo.append(h3UserName, pUserOcupation)
+    boxProfile.append(imgRecentProfilePic, profileInfo)
+
+    getRepos(parseItem.repos_url)
+    createMailTo(parseItem.email)
+
+    return boxProfile
+    
+}
+renderRecentProfiles() 
+
+async function getRepos(url) {
+
+    try {
+        const data = await fetch(url)
+        const dataJson = await data.json();
+        dataJson.forEach(item =>{
+            creatCard(item)
+        })
+
+    } catch {
+        console.log("Deu ruim!")
+    }
+}
+
+function createMailTo(email){
+    const buttonsNavDiv = document.getElementById('buttons-nav')
+
+    const buttonEmail = document.createElement('button')
+    buttonEmail.setAttribute('id', 'button-nav-email')
+    buttonEmail.setAttribute('onclick', `location.href='mailto:${email}'`)
+    buttonEmail.innerText = ('Email')
+
+    const buttonChangeUser = document.createElement('button')
+    buttonChangeUser.setAttribute('id', 'button-nav-change-user')
+
+    buttonChangeUser.addEventListener('click', () =>{
+        window.location.href='../../index.html'
+    })
+
+    buttonChangeUser.innerText = ('Trocar de usuário')
+
+    buttonsNavDiv.append(buttonEmail, buttonChangeUser)
+
+    return buttonsNavDiv
+}
+
+
+
